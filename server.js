e583e76ad37f22108f5bf6c16451e587d35e77eb
KislayAgrahari <kislaykumar41@gmail.com>
2016-11-11 00:56:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -37,8 +37,15 @@ app.get('/ui/main_bg.gif', function (req, res) {
 app.get('/ui/thinking-man.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'thinking-man.png'));
 });
+var pool=new pool(config);
 app.get('/test-db', function (req, res) {
-  
+  pool.query('SELECT*FROM test',function (err,res){
+      if(err){
+          res.status(500).send(err.toString());
+      }else{
+          res.send(JSON.stringify(result));
+      }
+  });
 });
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
