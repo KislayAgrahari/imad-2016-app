9cd9763602c4d1adcbed31dd7c960002840aa08a
KislayAgrahari <kislaykumar41@gmail.com>
2016-11-11 19:22:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var config = {
     database: 'kislayagrahari',
     host: 'db.imad.hasura-app.io',
     port: '5432',
-    password: process.env.DB_PASSWORD
+    password: db-kislayagrahari-70197
 };
 
 var app = express();
