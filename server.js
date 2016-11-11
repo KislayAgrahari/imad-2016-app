3fccce80cfb01ab3a7c6f7f9d323b00ce594eb08
KislayAgrahari <kislaykumar41@gmail.com>
2016-11-11 19:11:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,6 +17,19 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'kissintern.html'));
 });
+
+var pool=new pool(config);
+app.get('/test-db', function (req, res) {
+  pool.query('SELECT*FROM test',function (err,res){
+      if(err){
+          res.status(500).send(err.toString());
+      }else{
+          res.send(JSON.stringify(result));
+      }
+  });
+});
+
+
 app.get('/ui/kissintern.html', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'kissintern.html'));
 });
