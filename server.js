a59a36b7122403c76a1ff8762b3af0b088ef99ea
KislayAgrahari <kislaykumar41@gmail.com>
2016-11-12 18:48:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-var Pool = require('pg').Pool;
+/*var Pool = require('pg').Pool;
 
 var config = {
     user: 'kislayagrahari',
@@ -9,7 +9,7 @@ var config = {
     host: 'db.imad.hasura-app.io',
     port: '5432',
     password: db-kislayagrahari-70197
-};
+};*/
 
 var app = express();
 app.use(morgan('combined'));
@@ -18,16 +18,17 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'kissintern.html'));
 });
 
-var pool=new pool(config);
-app.get('/test-db', function (req, res) {
-  pool.query('SELECT*FROM test',function (err,result){
-      if(err){
-          res.status(500).send(err.toString());
-      }else{
-          res.send(JSON.stringify(result));
-      }
-  });
-});
+//var pool=new pool(config);
+//app.get('/test-db', function (req, res) {
+  //pool.query('SELECT*FROM test',function (err,result){
+    //  if(err){
+      //    res.status(500).send(err.toString());
+      //}
+     // else{
+       //   res.send(JSON.stringify(result));
+      //}
+ // });
+//});
 
 
 app.get('/ui/kissintern.html', function (req, res) {
